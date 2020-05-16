2ef59e1c0c7c6dc8f0af21c86604337bda0f5796
Jonas Hauser <Jonas.Hauser@swisscom.com>
2020-05-16 22:26:13 +0200

    Continue implementing all dynamic parts and start with local storage

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 function initApp() {
   'use strict';
 
-  console.log("initApp");
-
   const HAND_ITEMS = ['Schere', 'Stein', 'Papier', 'Brunnen', 'Streichholz'];
+  const HAND_ITEMS_REFERENCE = ['js-scissors-hand-button', 'js-rock-hand-button',
+    'js-paper-hand-button', 'js-fountain-hand-button', 'js-matchstick-hand-button'];
   const MAX_RANK_ITEMS = 10;
   const NEXT_ROUND_COUNTER_TIME = 3;
+  const DEFAULT_USER_NAME = "Anonymaus";
 
   let server = false;
 
-  let userHand = null;
-  let enemyHand = null;
-
-  // let playerName = "Anonymaus";
+  let playerName = DEFAULT_USER_NAME;
 
   const startPageContainer = document.getElementById('js-start-page');
   const changeServerButton = document.getElementById('js-change-server-button');
@@ -29,7 +27,7 @@ function initApp() {
   const nextRoundCounter = document.getElementById('js-next-round-counter');
   const userHandText = document.getElementById('js-user-hand');
   // const versusText = document.getElementById('js-next-round-counter');
-  const enemyHandText = document.getElementById('"js-enemy-hand');
+  const enemyHandText = document.getElementById('js-enemy-hand');
   const backToHomeButton = document.getElementById('js-back-to-ranking-button');
 
   const historyContainer = document.getElementById('js-history');
@@ -37,13 +35,32 @@ function initApp() {
 
   function initView() {
     console.log("initView");
-    HAND_ITEMS.forEach(function(item) {
-      // TODO implement buttons init View!
+    let buttons = document.createElement("div");
+    HAND_ITEMS_REFERENCE.forEach((referenceId, index) => {
+      let button = document.createElement("button");
+      button.setAttribute("id", referenceId);
+      button.innerText = HAND_ITEMS[index];
+      button.type = "button";
+      buttons.appendChild(button);
     });
+    handButtonsContainer.append(buttons);
+  }
+
+  function initLocalStorage() {
+    window.localStorage.setItem("history", JSON.stringify([]));
+    window.localStorage.setItem("userScores", JSON.stringify([]));
   }
 
   function updateView() {
     console.log("updateView");
+
+    let history = JSON.parse(window.localStorage.getItem("history"));
+    history.forEach(historyItem => {
+      console.log(historyItem);
+      //TODO implement history table update
+    })
+
+    //TODO implement ranking table update
   }
 
   // controller & helper functions
@@ -55,7 +72,9 @@ function initApp() {
 
   function showGame() {
     hideAllContainers();
-    // playerNameText.innerText = playerNameInput.value();
+    playerName = playerNameInput.value.length !== 0 ? playerNameInput.value : DEFAULT_USER_NAME;
+    playerNameText.innerText = playerName + "!";
+    updateLocalStorageItem("userScores", [playerName, 0]);
     playGameContainer.hidden = false;
     historyContainer.hidden = false;
   }
@@ -80,17 +99,90 @@ function initApp() {
     }
   }
 
+  function handSelect(event) {
+    let userHandReferenceId = event.target.id
+    let userHand = "";
+    HAND_ITEMS_REFERENCE.forEach((value, index) => {
+      if (userHandReferenceId === value) {
+        userHandText.innerText = HAND_ITEMS[index];
+        userHand = HAND_ITEMS[index];
+      }
+    })
+
+    if (server) {
+      //TODO implement server side
+    } else {
+      let randomHandReferenceId = getRandomAnswer();
+      let randomHand = HAND_ITEMS[HAND_ITEMS_REFERENCE.indexOf(randomHandReferenceId)];
+      enemyHandText.innerText = randomHand;
+      if (userHandReferenceId === randomHandReferenceId) {
+        userHandText.style.color = 'black';
+        enemyHandText.style.color = 'black';
+        updateLocalStorageItem("history", ["TIE", userHand, randomHand]);
+      } else {
+        if (checkIfUserWins(userHandReferenceId, randomHandReferenceId)) {
+          userHandText.style.color = 'green';
+          enemyHandText.style.color = 'red';
+          updateLocalStorageItem("history", ["LOSE", userHand, randomHand]);
+          //TODO implement updateUserScore
+        } else {
+          userHandText.style.color = 'red';
+          enemyHandText.style.color = 'green';
+          updateLocalStorageItem("history", ["WON", userHand, randomHand]);
+        }
+      }
+    }
+    updateView();
+  }
+
+  function checkIfUserWins(userChoice, computerChoice) {
+    if (userChoice === "js-scissors-hand-button") {
+      return computerChoice === "js-paper-hand-button" || computerChoice === "js-matchstick-hand-button";
+    }
+    if (userChoice === "js-rock-hand-button") {
+      return computerChoice === "js-scissors-hand-button" || computerChoice === "js-matchstick-hand-button";
+    }
+    if (userChoice === "js-paper-hand-button") {
+      return computerChoice === "js-rock-hand-button" || computerChoice === "js-fountain-hand-button";
+    }
+    if (userChoice === "js-fountain-hand-button") {
+      return computerChoice === "js-rock-hand-button" || computerChoice === "js-scissors-hand-button";
+    }
+    if (userChoice === "js-matchstick-hand-button") {
+      return computerChoice === "js-paper-hand-button" || computerChoice === "js-fountain-hand-button";
+    }
+    return false;
+  }
+
+  function updateLocalStorageItem(key, value) {
+    let currentValue = JSON.parse(window.localStorage.getItem(key));
+    currentValue.push(value);
+    window.localStorage.setItem(key, JSON.stringify(currentValue))
+  }
+
+  function getRandomAnswer() {
+    let min = Math.ceil(0);
+    let max = Math.floor(HAND_ITEMS.length - 1);
+    let randomHandIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+    return HAND_ITEMS_REFERENCE[randomHandIndex];
+  }
+
   async function changeToServer() {
     //TODO implement ajax call to backend and update ranking content
     return await new Promise(resolve => setTimeout(resolve, 1000));
   }
 
-  //attach eventListeners
+// init view / display initial state
+  initView();
+  initLocalStorage();
+  updateView();
+
+//attach eventListeners
   startGameButton.addEventListener('click', showGame)
   backToHomeButton.addEventListener('click', showStartPage);
   changeServerButton.addEventListener('click', changeServerState)
-
-  // init view / display initial state
-  initView();
-  updateView();
+  handButtonsContainer.querySelectorAll("button").forEach(element => {
+      element.addEventListener('click', handSelect)
+    }
+  )
 }
